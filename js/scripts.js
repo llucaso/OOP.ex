d35015b148b61537e780ee32f617ed3b7084dda3
Lukasz Redzinski <Lukasz Redzinski>
2017-04-11 01:33:26 +0200

    code upgrade

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@
 	this.cena = cena;
 	this.kolor = kolor;
 	this.bateria = bateria;
-}
+};
 
 Telefon.prototype.chargingCounter = function() {
 	var czasLadowania = this.bateria / 240;
 	console.log("Czas ładowania to " + czasLadowania + " jednostek.");
-}
+};
 
 Telefon.prototype.printInfo = function() {
 	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ".");
-}
+};
 
 var iPhone6s = new Telefon("Apple", 2250, "srebrny", 3500);	
 var SamsungGalaxyS6 = new Telefon("Samsung", 1700, "czerwony", 2500);
