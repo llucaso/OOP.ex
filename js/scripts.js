893f9479d4b3667c2928522547610513c65d34b6
llucaso <lredzinski@gmail.com>
2017-04-23 14:24:40 +0200

    contry searcher

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-﻿
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+var capitalCity = $('#capital');
+
+$('#search').click(searchCountries);
+
+function searchCountries () {
+	var countryName = $('#country-name').val();				//metoda val() wyszukiwanie wpisanej wartosci
+	if(!countryName.length) countryName = 'Poland';
+
+	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+function showCountriesList(resp) {
+  	countriesList.empty();
+  	capitalCity.empty();
+resp.forEach(function(item) {
+   		$('<li>').text(item.name).appendTo(countriesList);
+   		$('<li>').text(item.capital).appendTo(capitalCity);
+});
+}
+
+
