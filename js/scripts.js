0bd83de5f4f8568b01981fcc4d6a064f64746fb8
llucaso <lredzinski@gmail.com>
2017-04-28 12:54:07 +0200

    stuck in .js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,49 @@
+$(function() {
+
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 var capitalCity = $('#capital');
 
-$('#search').click(searchCountries);
+$('#search')
+  .click(searchCountries);
 
 function searchCountries () {
-	var countryName = $('#country-name').val();				//metoda val() wyszukiwanie wpisanej wartosci
-	if(!countryName.length) countryName = 'Poland';
+  var countryName = $('#country-name').val();                   
+  if(!countryName.length) countryName = 'Poland';                
 
-	$.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+  $.ajax({                                           
+      url: url + countryName,
+      method: 'GET',
+      success: showCountriesList
+    });
 }
+
+
+
+
+
 
 function showCountriesList(resp) {
-  	countriesList.empty();
-  	capitalCity.empty();
+    countriesList.empty();
+    capitalCity.empty();              //nie dziala
+
 resp.forEach(function(item) {
-   		$('<li>').text(item.name).appendTo(countriesList);
-   		$('<li>').text(item.capital).appendTo(capitalCity);
+      $('<li>')
+        .text(item.name)
+        .appendTo(countriesList);
+      //$('<li>').text(item.capital).appendTo(capitalCity);
+
+$(countriesList)
+  .one('mouseup', function() {
+        $('<li>')
+          .text(item.capital + item.name)
+          .appendTo(capitalCity);
+          
+
 });
+
+});
+
 }
 
-
+});
